7449dffeb26c6ac2b1fd5f458126fecdaf9027e2
Marty Pauley <kasei@cpan.org>
2001-12-16 17:50:36 +0000

    Import of KASEI/Crypt-Simple-0.04 from CPAN.
    gitpan-cpan-distribution: Crypt-Simple
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         KASEI/Crypt-Simple-0.04.tar.gz
    gitpan-cpan-author:       KASEI
    gitpan-cpan-maturity:     released

--- Simple.pm
+++ Simple.pm
@@ -1,5 +1,5 @@
 package Crypt::Simple;
-$Crypt::Simple::VERSION = '0.01';
+$Crypt::Simple::VERSION = '0.04';
 
 =head1 NAME
 
@@ -7,16 +7,74 @@ Crypt::Simple - encrypt stuff simply
 
 =head1 SYNOPSIS
 
-  use Crypt::Simple "maybe put a password here";
+  use Crypt::Simple;
   
-  my $data = encrypt($stuff);
+  my $data = encrypt(@stuff);
 
-  my $same_stuff = decrypt($data);
+  my @same_stuff = decrypt($data);
 
 =head1 DESCRIPTION
 
-This provides a simple way to encrypt stuff.  The ciphertext is suitable for
-sticking in HTTP cookies or email headers since it is base-64 encoded.
+Maybe you have a web application and you need to store some session data at the
+client side (in a cookie or hidden form fields) but you don't want the user to
+be able to mess with the data.  Maybe you want to save secret information to a
+text file.  Maybe you have better ideas of what to do with encrypted stuff!
+
+This little module will convert all your data into nice base64 text that you
+can save in a text file, send in an email, store in a cookie or web page, or
+bounce around the Net.  The data you encrypt can be as simple or as complicated
+as you like.
+
+=head1 KEY
+
+If you don't pass any options when using C<Crypt::Simple> we will generate a key
+for you based on the name of your module that uses this one.  In many cases this
+works fine, but you may want more control over the key.  Here's how:
+
+=over 4
+
+=item use Crypt::Simple passphrase => 'pass phrase';
+
+The MD5 hash of the text string "pass phrase" is used as the key.
+
+=item use Crypt::Simple prompt => 'Please type the magic words';
+
+The user is prompted to enter a passphrase, and the MD5 hash of the entered text
+is used as the key.
+
+=item use Crypt::Simple file => '/home/marty/noise';
+
+The contents of the file /home/marty/noise are used as the key.
+
+=back
+
+=head1 INTERNALS
+
+C<Crypt::Simple> is really just a wrapper round a few other useful Perl
+modules: you may want to read the documentation for these modules too.
+
+We use C<FreezeThaw> to squish all your data into a concise textual
+representation.  We use C<Compress::Zlib> to compress this string, and then use
+C<Crypt::Blowfish> in a home-brew CBC mode to perform the encryption.
+Somewhere in this process we also add a MD5 digest (using C<Digest::MD5>).
+Then we throw the whole thing through C<MIME::Base64> to produce a nice bit of
+text for you to play with.
+
+Decryption, obviously, is the reverse of this process.
+
+=head1 WARNING
+
+Governments throughout the world do not like encryption because it makes it
+difficult for them to look at all your stuff.  Each country has a different
+policy designed to stop you using encryption: some governments are honest enough
+to make it illegal; some think it is a dangerous weapon; some insist that you
+are free to encrypt, but only evil people would want to; some make confusing and
+contradictory laws because they try to do all of the above.
+
+Although this modules itself does not include any encryption code, it does use
+another module that contains encryption code, and this documentation mentions
+encryption.  Downloading, using, or reading this modules could be illegal where
+you live.
 
 =head1 AUTHOR
 
@@ -24,17 +82,15 @@ Marty Pauley E<lt>marty@kasei.comE<gt>
 
 =head1 COPYRIGHT
 
-  Copyright (C) 2001  Kasei
+  Copyright (C) 2001 Kasei Limited
 
-  This program is free software; you can redistribute it and/or modify it
-  under the terms of either:
-  a) the GNU General Public License;
-     either version 2 of the License, or (at your option) any later version.
-  b) the Perl Artistic License.
+  This program is free software; you can redistribute it and/or modify it under
+  the terms of the GNU General Public License; either version 2 of the License,
+  or (at your option) any later version.
 
-  This program is distributed in the hope that it will be useful, but
-  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-  or FITNESS FOR A PARTICULAR PURPOSE.
+  This program is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE.
 
 =cut
 
@@ -44,22 +100,20 @@ use Crypt::Blowfish;
 use Compress::Zlib;
 use MIME::Base64;
 use Digest::MD5 qw(md5);
+use FreezeThaw qw(freeze thaw);
 
 sub _chunk($) { $_[0] =~ /.{1,8}/ogs }
 
 sub import {
-	my ($class, @stuff) = @_;
+	my ($class, @args) = @_;
 	my $caller = caller;
-	my $stuff = join '', @stuff;
-	unless ($stuff) {
-		$stuff ||= "$0:$caller";
-		carp "using default encryption key";
-	}
-	my $cipher = Crypt::Blowfish->new(md5("$class:$stuff"));
+	my $key = $class->get_key_param(@args)
+		|| $class->get_key_default($caller);
+	my $cipher = Crypt::Blowfish->new($key);
 
 	no strict 'refs';
 	*{"${caller}::encrypt"} = sub {
-		my $data = $_[0];
+		my $data = freeze(@_);
 		my $sig = md5($data);
 		my $b0 = pack('NN', 0, 0);
 		my $ct = '';
@@ -83,10 +137,47 @@ sub import {
 		}
 		my $result = uncompress($pt);
 		croak "message digest incorrect" unless $sig eq md5($result);
-		return $result;
+		my @data = thaw($result);
+		return wantarray ? @data : $data[0];
 	};
 
       1;
+}
+
+sub get_key_param {
+	my ($class, @p) = @_;
+	my %p = @p;
+	my $key = '';
+	foreach my $k ($class->get_key_methods) {
+		next unless exists $p{$k};
+		if (my $m = $class->can("key_from_$k")) {
+			$key = $class->$m($p{$k});
+			last if $key;
+		}
+	}
+	return $key;
+}
+
+sub get_key_default {
+	my ($class, $c) = @_;
+	return md5("$class,$c");
+}
+
+sub get_key_methods { qw{passphrase file prompt} }
+
+sub key_from_passphrase {
+	my ($class, $pass) = @_;
+	return md5($pass);
+}
+
+sub key_from_file {
+	my ($class, $file) = @_;
+}
+
+sub key_from_prompt {
+	my ($class, $prompt) = @_;
+	my $pass = "foo"; # read from prompt
+	return $class->key_from_passphrase($pass);
 }
 
 1;
